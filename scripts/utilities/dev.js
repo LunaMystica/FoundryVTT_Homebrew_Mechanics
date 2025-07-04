e27b5729a363c220cf2865b733cf0307b322621d
LunaMystica <lenna.albers2@gmail.com>
2025-07-04 22:00:16 +0200

    Changed the script to not require debug mode to be toggled for it to function

--- scripts/utilities/dev.js
+++ scripts/utilities/dev.js
@@ -1,9 +1,6 @@
 async function log(messageContent) {
 	const debug = game.settings.get('xeno-homebrew-mechanics', 'debug-toggle');
 	const chat_messages = game.settings.get('xeno-homebrew-mechanics', 'chat-message-toggle');
-
-	if (!debug) return;
-
 	if (chat_messages) {
 		messageContent = messageContent.replace(/<\/h3><br>/g, '</h3>');
 		messageContent = messageContent.replace(/<hr><br>/g, '<hr>');
@@ -14,7 +11,7 @@ async function log(messageContent) {
 			whisper: ChatMessage.getWhisperRecipients('GM'),
 		});
 	}
-	if (debug) {
+	if (debug && !chat_messages) {
 		messageContent = messageContent.replace(/<br>/g, '\n');
 		messageContent = messageContent.replace(/<hr>/g, '');
 
